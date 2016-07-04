31768f0df1083819967857d843366b19ef86f0d9
Martin Geno <geno+dev@fireorbit.de>
2016-07-04 14:09:38 +0200

    gluon-respondd-airtime: MAKROs

--- ffhb/gluon-respondd-airtime/src/respondd.c
+++ ffhb/gluon-respondd-airtime/src/respondd.c
@@ -7,42 +7,32 @@
 
 #include "airtime.h"
 
+#if GLUON
+static const char const *wifi_0_dev = "client0";
+static const char const *wifi_1_dev = "client1";
+
+#else
+static const char const *wifi_0_dev = "wlan0";
+static const char const *wifi_1_dev = "wlan1";
+
+#endif /* GLUON */
+
+
 void fill_airtime_json(struct airtime_result *air, struct json_object* wireless){
 	struct json_object *ret = NULL, *obj = NULL;
 
 	obj = json_object_new_object();
 	if(!obj)
 		goto error;
+#define JSON_ADD_INT64(value,key) {ret = json_object_new_int64(value); if(!ret) goto error; json_object_object_add(obj,key,ret);}
+#define JSON_ADD_INT(value,key) {ret = json_object_new_int(value); if(!ret) goto error; json_object_object_add(obj,key,ret);}
+	JSON_ADD_INT64(air->active_time.current,"active")
+	JSON_ADD_INT64(air->busy_time.current,"busy")
+	JSON_ADD_INT64(air->rx_time.current,"rx")
+	JSON_ADD_INT64(air->tx_time.current,"tx")
 
-	ret = json_object_new_int64(air->active_time.current);
-	if(!ret)
-		goto error;
-	json_object_object_add(obj,"active",ret);
-
-	ret = json_object_new_int64(air->busy_time.current);
-	if(!ret)
-		goto error;
-	json_object_object_add(obj,"busy",ret);
-
-	ret = json_object_new_int64(air->rx_time.current);
-	if(!ret)
-		goto error;
-	json_object_object_add(obj,"rx",ret);
-
-	ret = json_object_new_int64(air->tx_time.current);
-	if(!ret)
-		goto error;
-	json_object_object_add(obj,"tx",ret);
-
-	ret = json_object_new_int(air->noise);
-	if(!ret)
-		goto error;
-	json_object_object_add(obj,"noise",ret);
-
-	ret = json_object_new_int(air->frequency);
-	if(!ret)
-		goto error;
-	json_object_object_add(obj,"frequency",ret);
+	JSON_ADD_INT(air->noise,"noise")
+	JSON_ADD_INT(air->frequency,"frequency")
 
 error:
 	if(air->frequency >= 2400  && air->frequency < 2500)
@@ -63,7 +53,7 @@ static struct json_object *respondd_provider_statistics(void) {
 	if (!ret)
 		return NULL;
 
-	a = get_airtime("client0","client1");
+	a = get_airtime(wifi_0_dev,wifi_1_dev);
 	if (!a)
 		goto end;
 
